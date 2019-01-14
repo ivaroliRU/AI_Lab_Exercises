442dce74dbc711b1d3b557079b39834b756e0e14
Ivar Óli <ivartheoli@gmail.com>
2019-01-14 19:11:13 +0000

    complete lab 1

--- lab1/src/VacuumCleaningAgent.java
+++ lab1/src/VacuumCleaningAgent.java
@@ -10,7 +10,7 @@ public class VacuumCleaningAgent implements Agent
 	int stage = 0;
 	//distance from right and bottom walls
 	int fromRight = 0, fromBottom = 0;
-	boolean movingColumns = false;
+	boolean movingColumns = false, startedCoveringRect = false;
 	
     public String nextAction(Collection<String> percepts) {
     	boolean isBump = false;
@@ -34,13 +34,19 @@ public class VacuumCleaningAgent implements Agent
     	case 2:
     		return findCorner(isBump); // finally find the corner
     	case 3:
-    		return coverRect(isBump);
+    		return coverRect(isBump); 
     	case 4:
     		return coverRect(isBump);
     	case 5:
-    		return goHome();
+    		return rightCornerBack(isBump);
+    	case 6:
+    		return rightCornerBack(isBump);
+    	case 7:
+    		return goHome(isBump);
+    	case 8:
+    		return goHome(isBump);
     	default:
-    		return "TURN_OFF";
+    		return actions[1];
     	}
 	}
     
@@ -57,9 +63,11 @@ public class VacuumCleaningAgent implements Agent
     		return turnRight();
     	}
     	else if(stage == 1 && dir == 'E') {
+    		fromRight++;
     		return actions[4];
     	}
     	else {
+    		fromBottom++;
     		return actions[4];
     	}
     }
@@ -69,7 +77,7 @@ public class VacuumCleaningAgent implements Agent
     //stage == 4 -> go south and turn right on a wall
     private String coverRect(boolean isBump) {
     	System.out.println("Covering Rect");
-    	if(isBump && !movingColumns) {
+    	if(isBump && !movingColumns && startedCoveringRect) {
     		System.out.println("we have a bump");
     		if(stage == 3) {
     			stage++;
@@ -80,6 +88,10 @@ public class VacuumCleaningAgent implements Agent
     	}
     	
     	if(stage == 3) {
+    		if(!startedCoveringRect){
+    			startedCoveringRect = true;
+    		}
+    		
     		if(dir != 'N' && !movingColumns) {
     			System.out.println("1.1");
     			return turnRight();
@@ -118,8 +130,65 @@ public class VacuumCleaningAgent implements Agent
     	return actions[4];
     }
     
-    private String goHome() {
-    	System.out.println("GoHome");
+    private String rightCornerBack(boolean isBump) {
+    	System.out.println("rightCornerBack");
+    	
+    	if(isBump) {
+    		stage++;
+    	}
+    	
+    	if(stage == 5) {
+    		if(dir != 'E') {
+    			if(dir == 'N') {
+    				return turnRight();
+    			}
+    			else if (dir == 'S') {
+    				return turnLeft();
+    			}
+    			else {
+    				return turnRight();
+    			}
+    		}
+    		else {
+    			return actions[4];
+    		}
+    	}
+    	else if(stage == 6) {
+    		if(dir != 'S') {
+    			return turnRight();
+    		}
+    		else {
+    			return actions[4];
+    		}
+    	}
+    	return actions[4];
+    }
+    
+    private String goHome(boolean isBump) {
+    	System.out.println("goHome");
+    	if(stage == 8) {
+    		if(dir != 'N') {
+				return turnLeft();
+			}
+    		
+    		if(fromBottom > 0) {
+    			fromBottom--;
+    			return actions[4];
+    		}
+    		stage++;
+    	}
+    	
+    	if(stage == 7){
+    		if(dir != 'W') {
+				return turnLeft();
+			}
+    		
+    		if(fromRight > 0) {
+    			fromRight--;
+    			return actions[4];
+    		}
+    	}
+    	
     	return actions[1];
     }
     
@@ -134,7 +203,7 @@ public class VacuumCleaningAgent implements Agent
     	else if(dir == 'S') {
     		dir = 'E';
     	}
-    	else {
+    	else if(dir == 'E') {
     		dir = 'N';
     	}
     	
@@ -152,7 +221,7 @@ public class VacuumCleaningAgent implements Agent
     	else if(dir == 'S') {
     		dir = 'W';
     	}
-    	else {
+    	else if(dir == 'W'){
     		dir = 'N';
     	}
     	
