88b74ab7deb9656c87738e9130371a24685f6517
Ivar Óli <ivartheoli@gmail.com>
2019-01-21 12:36:21 +0000

    state class created

---
+++ prog1/src/State.java
@@ -0,0 +1,11 @@
+public class State {
+	/* TODO
+	 * Implement methods to compute the legal moves in a state and the successor state
+	 * resulting of executing an action.
+	 */
+	
+	/* TODO
+	 * Implement the goal test, i.e., a method telling you whether a certain state fulfils all goal
+	 * conditions.
+	 */
+}
